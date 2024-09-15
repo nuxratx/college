4b42871ab80ef5a3e323dac7407178ba854d08b5
nuxratx <161661618+nuxratx@users.noreply.github.com>
2024-09-14 22:39:38 -0400

    initiate palindrome

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+palindrome.py
+
+'''Create a python script that will check if a particular string is a palindrome or not.  
+(Reads the same forwards and backwards). You can either pass the string as an argument or ask for user input
+A string is said to be a palindrome if the reverse of the string is the same as the string. For example, “radar” is a palindrome, but “radix” is not a palindrome '''
+
+
+
