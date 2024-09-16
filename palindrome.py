b0c87d9a758eff40300f482b0b060fcaa0d49b02
nuxratx <161661618+nuxratx@users.noreply.github.com>
2024-09-16 18:20:38 -0400

    done

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,16 @@
-palindrome.py
+'''Create a python script that will check if a particular string is a palindrome or not'''
 
-'''Create a python script that will check if a particular string is a palindrome or not.  
-(Reads the same forwards and backwards). You can either pass the string as an argument or ask for user input
-A string is said to be a palindrome if the reverse of the string is the same as the string. For example, “radar” is a palindrome, but “radix” is not a palindrome '''
+import argparse
 
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Find out if your word is a palindrome or not')
+parser.add_argument('-p', '--palindrome', type=str, required=True, help='your string goes here')
+args = parser.parse_args()
 
+# Define function to check if a string is a palindrome
+def is_palindrome_or_not(palindrome):
+    return palindrome == palindrome[::-1]
 
+# Call the function with the provided argument and print the result
+result = is_palindrome_or_not(args.palindrome)
+print('"{}" is a palindrome: {}'.format(args.palindrome, result))
