8ed2a334956c6eb4b7c4d3e49a7d1926a327f7e9
nuxratx <161661618+nuxratx@users.noreply.github.com>
2025-05-01 02:04:52 -0400

    init dns lab

--- computer-network-2025/DNSServer.py
+++ computer-network-2025/DNSServer.py
@@ -42,9 +42,9 @@ def decrypt_with_aes(encrypted_data, password, salt):
     decrypted_data = f.decrypt(encrypted_data)
     return decrypted_data.decode('utf-8')
 
-salt = b'salt' # Remember it should be a byte-object
-password = 'notavailable'
-input_string = 'SomeString'
+salt = b'Tandon' # Remember it should be a byte-object
+password = 'na4101@nyu.edu'
+input_string = 'AlwaysWatching'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
---
+++ computer-network-2025/subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 93802
+
+#problem 1
+p1Network = '38.252.183.112'
+p1Broadcast = '38.252.183.127'
+p1First = '38.252.183.113'
+p1Last =  '38.252.183.126'
+
+#problem 2
+p2Network = '205.237.145.128'
+p2Broadcast = '205.237.145.255'
+p2First = '205.237.145.129'
+p2Last = '205.237.145.254'
+
+#problem 3
+p3Network = '116.222.106.192'
+p3Broadcast = '116.222.106.199'
+p3First = '116.222.106.193'
+p3Last = '116.222.106.198'
+
+#problem 4
+p4Network = '27.207.68.192'
+p4Broadcast = '27.207.68.255'
+p4First = '27.207.68.193'
+p4Last = '27.207.68.254'
+
+#problem 5
+p5Network = '194.193.30.0'
+p5Broadcast = '194.193.30.255'
+p5First = '194.193.30.1'
+p5Last = '194.193.30.254'
+
+#problem 6
+p6Network = '105.178.248.64'
+p6Broadcast = '105.178.248.95'
+p6First = '105.178.248.65'
+p6Last =  '105.178.248.94'
+
+#problem 7
+p7Network = '16.163.209.0'
+p7Broadcast = '16.163.209.127'
+p7First = '16.163.209.1'
+p7Last = '16.163.209.126'
+
+#problem 8
+p8Network = '183.148.171.144'
+p8Broadcast = '183.148.171.159'
+p8First = '183.148.171.145'
+p8Last = '183.148.171.158'
+
+#problem 9
+p9Network = '94.133.133.192'
+p9Broadcast = '94.133.133.255'
+p9First = '94.133.133.193'
+p9Last = '94.133.133.254'
+
+#problem 10
+p10Network = '5.118.95.240'
+p10Broadcast = '5.118.95.247'
+p10First = '5.118.95.241'
+p10Last = '5.118.95.246'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
